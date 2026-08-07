39d18ab7493ff7d075c356ef7d06b5ddc2e64ad3
Rafael Aquino <rafael.acerqueirah@gmail.com>
2026-08-07 15:58:29 -0300

    feat(api): add document use cases and ports

---
+++ apps/api/src/supportops_api/application/__init__.py
@@ -0,0 +1,25 @@
+from supportops_api.application.documents import (
+    ActivateDocument,
+    CreateDocument,
+    CreateDocumentInput,
+    DeactivateDocument,
+    DocumentNotFoundError,
+    DocumentProcessor,
+    DocumentRepository,
+    GetDocument,
+    ListDocuments,
+    ProcessDocument,
+)
+
+__all__ = [
+    "ActivateDocument",
+    "CreateDocument",
+    "CreateDocumentInput",
+    "DeactivateDocument",
+    "DocumentNotFoundError",
+    "DocumentProcessor",
+    "DocumentRepository",
+    "GetDocument",
+    "ListDocuments",
+    "ProcessDocument",
+]
